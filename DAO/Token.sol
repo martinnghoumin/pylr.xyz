900fbd2f4f15cc47c2616d0603816726b8bcf331
martinnghoumin <84618001+martinnghoumin@users.noreply.github.com>
2022-11-05 22:29:54 -0700

    Update Token.sol

--- DAO/Token.sol
+++ DAO/Token.sol
@@ -1,35 +1,3 @@
-/*
-This file is part of the DAO.
-
-The DAO is free software: you can redistribute it and/or modify
-it under the terms of the GNU lesser General Public License as published by
-the Free Software Foundation, either version 3 of the License, or
-(at your option) any later version.
-
-The DAO is distributed in the hope that it will be useful,
-but WITHOUT ANY WARRANTY; without even the implied warranty of
-MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-GNU lesser General Public License for more details.
-
-You should have received a copy of the GNU lesser General Public License
-along with the DAO.  If not, see <http://www.gnu.org/licenses/>.
-*/
-
-
-/*
-Basic, standardized Token contract with no "premine". Defines the functions to
-check token balances, send tokens, send tokens on behalf of a 3rd party and the
-corresponding approval process. Tokens need to be created by a derived
-contract (e.g. TokenCreation.sol).
-
-Thank you ConsenSys, this contract originated from:
-https://github.com/ConsenSys/Tokens/blob/master/Token_Contracts/contracts/Standard_Token.sol
-Which is itself based on the Ethereum standardized contract APIs:
-https://github.com/ethereum/wiki/wiki/Standardized_Contract_APIs
-*/
-
-/// @title Standard Token Contract.
-
 pragma solidity ^0.4.6;
 
 contract Plutocracy {
