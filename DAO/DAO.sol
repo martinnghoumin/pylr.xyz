71c1f0f126e6fee52b110ee66df85f16dada8ea7
martinnghoumin <84618001+martinnghoumin@users.noreply.github.com>
2022-11-05 19:34:30 -0700

    Update DAO.sol

--- DAO/DAO.sol
+++ DAO/DAO.sol
@@ -1,33 +1,10 @@
-/*
-This file is part of the DAO.
-
-The DAO is free software: you can redistribute it and/or modify
-it under the terms of the GNU lesser General Public License as published by
-the Free Software Foundation, either version 3 of the License, or
-(at your option) any later version.
-
-The DAO is distributed in the hope that it will be useful,
-but WITHOUT ANY WARRANTY; without even the implied warranty of
-MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-GNU lesser General Public License for more details.
-
-You should have received a copy of the GNU lesser General Public License
-along with the DAO.  If not, see <http://www.gnu.org/licenses/>.
-*/
-
-
-/*
-Standard smart contract for a Decentralized Autonomous Organization (DAO)
-to automate organizational governance and decision-making.
-*/
-
 import "./TokenCreation.sol";
 
 pragma solidity ^0.4.4;
 
 contract DAOInterface {
-    // The minimum debate period that a generic proposal can have
-    uint constant minProposalDebatePeriod = 2 weeks;
+    // The minimum voting period that a generic milestone disbursment can have
+    uint constant minProposalDebatePeriod = 3 days;
     // The minimum debate period that a split proposal can have
     uint constant quorumHalvingPeriod = 25 weeks;
     // Period after which a proposal is closed
